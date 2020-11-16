b498c5c2724d4b5766d8e6496912e5c0292ccdb8
Calvin <calvinho125@gmail.com>
2020-11-16 13:14:46 -0500

    New

--- routes/books.js
+++ routes/books.js
@@ -1,72 +1,185 @@
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Book:
+ *        type: object
+ *        required:
+ *          - title
+ *          - author
+ *          - finished
+ *        properties:
+ *          id:
+ *           type: integer
+ *           description: The auto-generated id of the book.
+ *          title:
+ *            type: string
+ *            description: The title of your book.
+ *          author:
+ *            type: string
+ *            description: Who wrote the book?
+ *          finished:
+ *            type: boolean
+ *            description: Have you finished reading it?
+ *          createdAt:
+ *            type: string
+ *            format: date
+ *            description: The date of the record creation.
+ *        example:
+ *           title: The Pragmatic Programmer
+ *           author: Andy Hunt / Dave Thomas
+ *           finished: true
+ */
+
+/**
+ * @swagger
+ * tags:
+ * 	name: Books
+ * 	description: API to manage your books.
+ */
+
 const express = require("express");
 const router = express.Router();
 
 const books = require("../util/data");
 
+/**
+ * @swagger
+ * path:
+ * books/:
+ *   get:
+ *     description: Lists all the books
+ *     tags:
+ */
 router.get("/", function (req, res) {
-	res.status(200).json(books);
+  res.status(200).json(books);
 });
 
+/**
+ * @swagger
+ * path:
+ * /{id}:
+ *   get:
+ *     summary: Gets a book by Id
+ *     tags: [Books]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *          required: true
+ *          description: The book id
+ *     responses:
+ *       202:
+ *         description: The list of books.
+ *         content:
+ *           application/json:
+ *       404:
+ *          description: Book not found.
+ */
 router.get("/:id", function (req, res) {
-	let book = books.find(function (item) {
-		return item.id == req.params.id;
-	});
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
 
-	book ? res.status(200).json(book) : res.sendStatus(404);
+  book ? res.status(200).json(book) : res.sendStatus(404);
 });
 
+/**
+ * @swagger
+ * path:
+ * /:
+ *   post:
+ *     summary: Creates a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *     responses:
+ *       201:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ */
 router.post("/", function (req, res) {
-	const { title, author, finished } = req.body;
+  const { title, author, finished } = req.body;
 
-	let book = {
-		id: books.length + 1,
-		title: title,
-		author: author,
-		finished: finished !== undefined ? finished : false,
-		createdAt: new Date(),
-	};
+  let book = {
+    id: books.length + 1,
+    title: title,
+    author: author,
+    finished: finished !== undefined ? finished : false,
+    createdAt: new Date(),
+  };
 
-	books.push(book);
+  books.push(book);
 
-	res.status(201).json(book);
+  res.status(201).json(book);
 });
 
+/**
+ * @swagger
+ * path:
+ * /{id}:
+ *   put:
+ *     summary: Update a book
+ *     tags: [Books]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: The book id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *     responses:
+ *       204:
+ *         description: Update was successful.
+ *       404:
+ *         description: Book not found.
+ */
 router.put("/:id", function (req, res) {
-	let book = books.find(function (item) {
-		return item.id == req.params.id;
-	});
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
 
-	if (book) {
-		const { title, author, finished } = req.body;
+  if (book) {
+    const { title, author, finished } = req.body;
 
-		let updated = {
-			id: book.id,
-			title: title !== undefined ? title : book.title,
-			author: author !== undefined ? author : book.author,
-			finished: finished !== undefined ? finished : book.finished,
-			createdAt: book.createdAt,
-		};
+    let updated = {
+      id: book.id,
+      title: title !== undefined ? title : book.title,
+      author: author !== undefined ? author : book.author,
+      finished: finished !== undefined ? finished : book.finished,
+      createdAt: book.createdAt,
+    };
 
-		books.splice(books.indexOf(book), 1, updated);
+    books.splice(books.indexOf(book), 1, updated);
 
-		res.sendStatus(204);
-	} else {
-		res.sendStatus(404);
-	}
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.delete("/:id", function (req, res) {
-	let book = books.find(function (item) {
-		return item.id == req.params.id;
-	});
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
 
-	if (book) {
-		books.splice(books.indexOf(book), 1);
-	} else {
-		return res.sendStatus(404);
-	}
+  if (book) {
+    books.splice(books.indexOf(book), 1);
+  } else {
+    return res.sendStatus(404);
+  }
 
-	res.sendStatus(204);
+  res.sendStatus(204);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express"),
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUI = require("swagger-ui-express");
 
 const app = express();
 app.use(
@@ -12,6 +14,41 @@ app.use(bodyParser.json());
 app.use("/books", require("./routes/books"));
 
 const PORT = process.env.PORT || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true })
+);
+
 app.listen(PORT);
 
 console.debug("Server listening on port: " + PORT);
