f141be3c9adcc2853e8d0f66c6bb4c4605037728
Calvin <calvinho125@gmail.com>
2020-11-28 02:22:08 -0500

    t

---
+++ shScripts/start_container.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Remember to change back to ec2-user
+cd /var/www/server
+
+docker-compose up -d
---
+++ shScripts/start_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo amazon-linux-extras install docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+sudo service docker start
+cd home/ec2-user/server
+
+docker-compose up -d
